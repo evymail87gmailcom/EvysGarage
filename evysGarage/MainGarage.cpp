346f57f83811817f910e9f2ff07d9863f6eb05f2
Evy Svensson <evymail87@gmail.com>
2020-01-03 10:48:21 +0100

    cleared some includes

--- evysGarage/MainGarage.cpp
+++ evysGarage/MainGarage.cpp
@@ -1,10 +1,5 @@
 #include<iostream>
-#include<string>
-#include <vector>
-#include"GarageHeader.h"
-#include"Vehicle.h"
 #include "AdminGarage.h"
-using namespace std;
 int main() {
 	AdminGarage start;
 	start.startAdmin();
